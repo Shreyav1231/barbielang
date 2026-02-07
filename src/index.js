b4cb0bc018b6e924e9e6d6d8ad23c29555d939a3
Kartavya Vashishtha <sendtokartavya@gmail.com>
2026-02-07 16:18:59 -0500

    override gear with sparkles fr this time

--- src/index.js
+++ src/index.js
@@ -16,84 +16,75 @@ import './index.css';
 Blockly.common.defineBlocks(blocks);
 Object.assign(javascriptGenerator.forBlock, forBlock);
 
-// Custom Sparkle Mutator Icon - replaces the gear icon with sparkles
-class SparkleMutatorIcon extends Blockly.icons.MutatorIcon {
-  initView(pointerdownListener) {
-    if (this.svgRoot) return;
-    super.initView(pointerdownListener);
+// Monkey-patch MutatorIcon to replace the gear with sparkles
+const originalInitView = Blockly.icons.MutatorIcon.prototype.initView;
+Blockly.icons.MutatorIcon.prototype.initView = function (pointerdownListener) {
+  // Call original to set up svgRoot and event listeners
+  originalInitView.call(this, pointerdownListener);
 
-    // Remove the default gear icon elements
-    while (this.svgRoot.firstChild) {
-      this.svgRoot.removeChild(this.svgRoot.firstChild);
-    }
-
-    // Add a pink background circle
-    Blockly.utils.dom.createSvgElement(
-      Blockly.utils.Svg.CIRCLE,
-      {
-        class: 'blocklyIconShape',
-        r: '8',
-        cx: '8',
-        cy: '8',
-        fill: '#FF69B4',
-      },
-      this.svgRoot
-    );
-
-    // Add the sparkles icon (scaled down from 24x24 to fit 16x16)
-    const sparklesGroup = Blockly.utils.dom.createSvgElement(
-      Blockly.utils.Svg.G,
-      {
-        transform: 'scale(0.6) translate(1, 1)',
-      },
-      this.svgRoot
-    );
-
-    // Main sparkle shape
-    Blockly.utils.dom.createSvgElement(
-      Blockly.utils.Svg.PATH,
-      {
-        d: 'M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z',
-        fill: 'white',
-        stroke: 'none',
-      },
-      sparklesGroup
-    );
-
-    // Small cross sparkle (top right)
-    Blockly.utils.dom.createSvgElement(
-      Blockly.utils.Svg.PATH,
-      {
-        d: 'M20 2v4M22 4h-4',
-        fill: 'none',
-        stroke: 'white',
-        'stroke-width': '2',
-        'stroke-linecap': 'round',
-      },
-      sparklesGroup
-    );
-
-    // Small circle sparkle (bottom left)
-    Blockly.utils.dom.createSvgElement(
-      Blockly.utils.Svg.CIRCLE,
-      {
-        cx: '4',
-        cy: '20',
-        r: '2',
-        fill: 'white',
-      },
-      sparklesGroup
-    );
+  // Remove the default gear icon elements
+  while (this.svgRoot.firstChild) {
+    this.svgRoot.removeChild(this.svgRoot.firstChild);
   }
-}
 
-// Register the custom sparkle icon to replace the default mutator icon
-Blockly.registry.register(
-  Blockly.registry.Type.ICON,
-  Blockly.icons.MutatorIcon.TYPE.toString(),
-  SparkleMutatorIcon,
-  true
-);
+  // Add a pink background rounded rect
+  Blockly.utils.dom.createSvgElement(
+    Blockly.utils.Svg.RECT,
+    {
+      class: 'blocklyIconShape',
+      rx: '4',
+      ry: '4',
+      height: '16',
+      width: '16',
+      fill: '#FF69B4',
+    },
+    this.svgRoot
+  );
+
+  // Add the sparkles icon (scaled down from 24x24 to fit ~14x14 centered)
+  const sparklesGroup = Blockly.utils.dom.createSvgElement(
+    Blockly.utils.Svg.G,
+    {
+      transform: 'translate(1, 1) scale(0.58)',
+    },
+    this.svgRoot
+  );
+
+  // Main sparkle shape (4-pointed star)
+  Blockly.utils.dom.createSvgElement(
+    Blockly.utils.Svg.PATH,
+    {
+      d: 'M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z',
+      fill: 'white',
+    },
+    sparklesGroup
+  );
+
+  // Small cross sparkle (top right)
+  Blockly.utils.dom.createSvgElement(
+    Blockly.utils.Svg.PATH,
+    {
+      d: 'M20 2v4M22 4h-4',
+      fill: 'none',
+      stroke: 'white',
+      'stroke-width': '2',
+      'stroke-linecap': 'round',
+    },
+    sparklesGroup
+  );
+
+  // Small circle sparkle (bottom left)
+  Blockly.utils.dom.createSvgElement(
+    Blockly.utils.Svg.CIRCLE,
+    {
+      cx: '4',
+      cy: '20',
+      r: '2',
+      fill: 'white',
+    },
+    sparklesGroup
+  );
+};
 
 // Rename "if" block to "feel" block
 Blockly.Msg['CONTROLS_IF_MSG_IF'] = 'feel';
